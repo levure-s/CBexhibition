51a3398828c80971f83feff5a3846af8e702df8f
levure-s <levure.sauvage@gmail.com>
2021-02-07 17:08:49 +0900

    first_commit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1086175.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': true,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 42209678, 42209694, 42209700, 42209670, 42209641, 42209713, 42209711, 42209645, 42209680, 42209687 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/42209741/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/42209627/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/42209621/1/ammo.js', 'preload' : true},
+];
